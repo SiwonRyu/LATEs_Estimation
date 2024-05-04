2146108c4e7f14ed7bfa6f68a415f454d77a9234
Siwon <siwonryu91@gmail.com>
2024-05-04 13:35:09 -0500

    v240504

---
+++ Matlab/test_estimation.m
@@ -0,0 +1,16 @@
+clc
+clear all
+format compact
+format short g
+
+addpath('mfiles')
+% Set monotone pair
+z_a = 2;
+z_b = 4;
+
+% Load dataset 
+load('test_data.mat')
+
+% Estimate
+result = estim(Y,D,Z,W,T,[],[],z_a,z_b,"true","lin",1,"on");
+result
